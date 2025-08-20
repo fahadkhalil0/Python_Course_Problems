c8d88654ca15d3c4cf81f04ee611c001567eb5d7
Fahad Khalil <fahadfiverr.0@gmail.com>
2025-08-20 17:40:39 +0500

    Problem no 7 solved.

--- Chapter8/08_practiceset/07_practice.py
+++ Chapter8/08_practiceset/07_practice.py
@@ -1,2 +1,11 @@
-#practice set no -7
+#practice set no 07
+# remove a word from list and also use the strip() method/function.
 
+def remove(l, word):
+    n = []
+    for item in l:
+        if not(item == word):
+            n.append(item.strip(word))
+    return n
+l = ["Ali", "Awais", "Sara", "Tariq", "Subhan", "an"]
+print(remove(l, "an"))
