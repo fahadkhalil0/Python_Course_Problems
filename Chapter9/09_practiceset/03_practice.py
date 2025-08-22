c6189578e2c6a2d037ad8a7ee3371c3005175186
Fahad Khalil <fahadfiverr.0@gmail.com>
2025-08-22 18:00:21 +0500

    Practice Question3 Added

---
+++ Chapter9/09_practiceset/03_practice.py
@@ -0,0 +1,13 @@
+#Practice task no 03
+
+#generate the tables from 1 to 20
+def generateTable(n):
+    table = ""
+    for i in range(1,11):
+        table += f"{n} x {i} = {n*i}\n"
+
+    with open(f"tables/table_{n}.txt", "w") as f:
+        f.write(table)
+
+for i in range (2,21):
+    generateTable(i)
