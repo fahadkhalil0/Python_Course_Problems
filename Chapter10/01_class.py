623298f005580a4a52ed91ca2222fe299b8a7693
Fahad Khalil <fahadfiverr.0@gmail.com>
2025-08-23 17:17:11 +0500

    Added Class Concept

---
+++ Chapter10/01_class.py
@@ -0,0 +1,19 @@
+# Class in python (OOP)
+
+class Employee:
+    name = "Fahad"
+    lang = "Py"
+    salary = 25000
+
+
+Fahad = Employee()
+Fahad.name = "Ali"
+print(Fahad.name, Fahad.lang, Fahad.salary)
+
+Fahad2 = Employee()
+print(Fahad2.name, Fahad2.lang, Fahad2.salary)
+
+Fahad3 = Employee()
+print(Fahad3.name, Fahad3.lang, Fahad3.salary)
+
+#here name is the attribute of instance and the other attributes are belong to the class.
