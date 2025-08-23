67666f80dffc4cc70ea2d304560db39ccec499f0
Fahad Khalil <fahadfiverr.0@gmail.com>
2025-08-23 17:23:44 +0500

    Practice_06 Added

---
+++ Chapter10/10_practiceset/06_practice.py
@@ -0,0 +1,13 @@
+# Can we actually change the self parameter into other parameter?
+
+class self:
+    def __init__(self, name):
+        self.name = name
+
+    def greet(greet):
+        print(f"Hello, {greet.name}!")
+
+Self = self("Fahad Khalil")
+Self.greet()
+
+#Yes we can do it.
