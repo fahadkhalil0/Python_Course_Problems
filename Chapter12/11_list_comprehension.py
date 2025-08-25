fe91b4f8bde5f89dc531fd4ae9e7fb78e74db46f
Fahad Khalil <fahadfiverr.0@gmail.com>
2025-08-25 18:23:29 +0500

    Implemented logically the term of List comprehension in Python

---
+++ Chapter12/11_list_comprehension.py
@@ -0,0 +1,16 @@
+# List comprehension in python
+
+list = [2,5,8,9,10,15]
+
+squarelist = []
+# for i in list:
+#     squarelist.append(i*i)
+#     # print(MyList)
+
+#List comprehension method used here.
+squarelist = [i*i for i in list]
+#Lets print the list with the square of each list element.
+print(list)
+print(squarelist)
+
+
