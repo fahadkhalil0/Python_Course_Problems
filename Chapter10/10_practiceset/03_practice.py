0d84f6c2dd4496b1a0dbe261c458e89dd1d150d3
Fahad Khalil <fahadfiverr.0@gmail.com>
2025-08-25 18:04:05 +0500

    Updated practice task no3 file

--- Chapter10/10_practiceset/03_practice.py
+++ Chapter10/10_practiceset/03_practice.py
@@ -3,8 +3,7 @@
 class Att:
     a = 10
 
-
 Value = Att()
 V = Value.a = 4
 print(V)
-print(Att.a) #No the class attribute will remain the same as it was!
+print(Att.a) #The answer is (No the class attribute will remain the same as it was!)
