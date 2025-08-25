55ce94587a87b01c1c424f5056af26bb7c2a250f
Fahad Khalil <fahadfiverr.0@gmail.com>
2025-08-25 18:14:53 +0500

    Implemented the logics of type defination in the Chapter12

---
+++ Chapter12/02_type_defination.py
@@ -0,0 +1,18 @@
+#Type Defination in python
+a : int = 10
+b : int = 100
+c = a + b
+print(c)
+
+#Type Defination in Python
+name : str = "fahad"
+dept : str = "IT"
+print(f"Name: {name} \nDept: {dept}")
+
+#Type defination in function
+#Function Defination
+def sum(a: int, b:int) -> int:
+    c =  a + b
+    return c
+#Function Calling
+print(sum(100,2))
